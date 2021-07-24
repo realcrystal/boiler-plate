a66e5d4b2266424a4b2ca72c2da52281ce3b335c
realcrystal <realcrystal98@gmail.com>
2021-07-25 00:51:54 +0900

    Add password encryption

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
 // const bodyParser = require('body-parser')
-const { User } = require('./models/User')
-const config = require('./config/key')
+// is deprecated
+const { User } = require('./models/User');
+const config = require('./config/key');
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
-app.use(express.json())
+app.use(express.json());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
 app.post('/register', (req, res) => {
-  const user = new User(req.body)
+  const user = new User(req.body);
   user.save((err, userInfo) => {
-    if(err) return res.json({success: false, err})
-    return res.status(200).json({success: true})
-  })
-})
+    if(err) return res.json({success: false, err});
+    return res.status(200).json({success: true});
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
